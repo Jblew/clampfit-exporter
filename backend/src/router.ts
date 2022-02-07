c34036fbb59f3839687dac2f3a6e52d276edb2be
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-02-07 22:58:37 +0100

    feat(backend): add_sample and get samples

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { envMust } from "./utils";
 import { authOr403 } from "@/auth";
+import { getConnection, getRepository } from "typeorm";
+import { PatchSample } from "./entity/PatchSample";
 
 const frontendRedirectURL = envMust("FRONTEND_REDIRECT_URL");
 export function getRoutes() {
@@ -15,6 +17,24 @@ export function getRoutes() {
 
   router.get("/profile", authOr403(), (req, res) => {
     res.send(JSON.stringify(req.oidc.user));
+  });
+
+  router.post("/add_sample", authOr403(), (req, res) => {
+    console.log(req.body);
+    console.log(req.params);
+    res.send(JSON.stringify(req.body));
+  });
+
+  router.get("/samples", authOr403(), async (req, res) => {
+    const email = req.oidc.user!.email;
+    if (!email) {
+      return res.status(500).send("Missing email in oidc claims");
+    }
+    const samples = await getRepository(PatchSample).find({
+      where: { email: email },
+      order: { date: "DESC" },
+    });
+    res.send(JSON.stringify({ samples }));
   });
 
   return router;
