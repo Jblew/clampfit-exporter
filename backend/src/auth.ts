1dddc0b75e968c61a793a7eee29deec072c222f3
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-02-07 21:55:30 +0100

    feat: always require auth on backend

---
+++ backend/src/auth.ts
@@ -0,0 +1,28 @@
+import { auth } from "express-openid-connect";
+import { RequestHandler } from "express";
+import { envMust } from "./utils";
+
+export function getAuthMiddleware() {
+  return auth({
+    authRequired: true,
+    auth0Logout: true,
+    secret: envMust("OIDC_AUTH_SECRET"),
+    baseURL: envMust("BASE_URL"),
+    clientID: envMust("OIDC_CLIENT_ID"),
+    issuerBaseURL: envMust("OIDC_ISSUER_BASEURL"),
+    routes: {
+      callback: "auth_callback",
+    },
+  });
+}
+
+export function authOr403(): RequestHandler {
+  return function (req, res, next) {
+    if (!req.oidc.isAuthenticated()) {
+      res.status(403).send({
+        error: `Authentication required. Redirect user to login endpoint`,
+      });
+    }
+    next();
+  };
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { getRoutes } from "./router";
+import { getAuthMiddleware } from "@/auth";
 import morgan from "morgan";
 import { initDatabase } from "./db";
 import { envMust } from "./utils";
@@ -19,6 +20,7 @@ async function run() {
   app.use(
     morgan(":method :url :status :res[content-length] - :response-time ms")
   );
+  app.use(routeBase, getAuthMiddleware());
   app.use(routeBase, getRoutes());
 
   app.listen(port, () => {
