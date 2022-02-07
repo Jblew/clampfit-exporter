7dc68d3d6684be05186cc503023bab763e428e19
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-02-07 23:23:54 +0100

    feat(backend): saving sample

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -3,6 +3,7 @@ import { envMust } from "./utils";
 import { authOr403 } from "@/auth";
 import { getRepository } from "typeorm";
 import { PatchSample } from "./entity/PatchSample";
+import { parseClampfitSummary } from "./domain";
 
 const frontendRedirectURL = envMust("FRONTEND_REDIRECT_URL");
 export function getRoutes() {
@@ -19,10 +20,26 @@ export function getRoutes() {
     res.send(JSON.stringify(req.oidc.user));
   });
 
-  router.post("/add_sample", authOr403(), (req, res) => {
-    console.log("body=", req.body);
-    console.log("params=", req.params);
-    res.send(JSON.stringify(req.body));
+  router.post("/add_sample", authOr403(), async (req, res) => {
+    try {
+      const email = req.oidc.user!.email;
+      if (!email) {
+        return res.status(500).send("Missing email in oidc claims");
+      }
+      const name = req.body.name || "";
+      const clampfitPaste = req.body.clampfitPaste;
+      if (!clampfitPaste) {
+        return res.status(400).send("Missing clampfitPaste");
+      }
+      const clampfitSample = parseClampfitSummary(clampfitPaste);
+      clampfitSample.name = name;
+      clampfitSample.email = email;
+      await getRepository(PatchSample).save(clampfitSample);
+      res.send(JSON.stringify({ ok: true }));
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Server error");
+    }
   });
 
   router.get("/samples", authOr403(), async (req, res) => {
