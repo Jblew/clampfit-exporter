bac9e67fce839bdb32ffd4d3b609acc658281fee
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-02-07 22:24:16 +0100

    fix: backend redirect

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,14 +1,20 @@
 import express from "express";
 import { envMust } from "./utils";
+import { authOr403 } from "@/auth";
 
+const frontendRedirectURL = envMust("FRONTEND_REDIRECT_URL");
 export function getRoutes() {
   const router = express.Router();
   router.get("/", (_req, res) => {
-    res.send({ ok: true });
+    res.redirect(frontendRedirectURL);
   });
 
   router.get("/health", (req, res) => {
     res.send({ ok: true });
+  });
+
+  router.get("/profile", authOr403(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
   });
 
   return router;
